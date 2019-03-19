a94e6bd00e126bdfb30788ab820b9ba28c013b4c
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-03-19 16:06:30 -0600

    day 3 y 4
    changes for day 3 and 4

---
+++ src/TenXamarinD3/TenXamarinD3/TenXamarinD3/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenXamarinD3.Model;
+using Xamarin.Forms;
+
+namespace TenXamarinD3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckIfShouldBeEnabled()
+        {
+            saveButton.IsEnabled = false;
+            if (!string.IsNullOrWhiteSpace(titleEntry.Text) && !string.IsNullOrWhiteSpace(contentEditor.Text))
+                saveButton.IsEnabled = true;
+        }
+
+        private void TitleEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void ContentEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            titleEntry.Text = string.Empty;
+            contentEditor.Text = string.Empty;
+
+            Experience newExperience = new Experience()
+            {
+                Title = titleEntry.Text,
+                Content = contentEditor.Text,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+            var inserted = 0;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Experience>();
+                inserted = conn.Insert(newExperience);
+            }
+
+            if(inserted > 0)
+            {
+                titleEntry.Text = string.Empty;
+                contentEditor.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Error", "There was an error inserting the experience", "ok");
+            }
+        }
+    }
+}
